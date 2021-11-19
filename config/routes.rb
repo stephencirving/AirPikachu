b978d46a29b2b7bc79fbd624dfcb20113dfeb217
stephencirving <stephen.c.irving@gmail.com>
2021-11-19 21:31:54 +1100

    4.5 added user fields to table and amended  profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users,
             path: '',
-            path_names: {
-              sign_in: 'login',
-              sign_out: 'logout',
-              edit: 'profile',
-              sign_up: 'registration'
-            },
-            controllers: {
-              omniauth_callbacks: 'users/omniauth_callbacks'
-            }
+            path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+            controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+
+  resources :users, only: [:show]
 end
