55c68e6244172d31d54f02c872724a2f89d6a10c
stephencirving <stephen.c.irving@gmail.com>
2021-11-23 23:48:56 +1100

    8.2 (no 8.1) Create Room Index Page - created personal listing page - index.html.erb, room.rb

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,4 +7,12 @@ class Room < ApplicationRecord
   validates :accommodate, presence: true
   validates :bed_room, presence: true
   validates :bath_room, presence: true
+
+  def cover_photo(size)
+    if self.photos.length > 0
+      self.photos[0].image.url(size)
+    else
+      "blank.jpg"
+    end
+  end
 end
