a627e44cc21f67ee5302cb3b6827f7707489c97a
stephencirving <stephen.c.irving@gmail.com>
2021-12-01 23:06:42 +1100

    10.3 AJAX for Start Date - _form.html.erb, rooms_controller.rb, routes.rb

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -57,6 +57,14 @@
     redirect_back(fallback_location: request.referer)
   end
 
+# --- Reservations ---
+def preload
+  today = Date.today
+  reservations = @room.reservations.where("start_date >= ? OR end_date >= ?", today, today)
+
+  render json: reservations
+end
+
 private
   def set_room
     @room = Room.find(params[:id])
