74bf111f3c84a605f62a4b4a462a76dedb33efe4
stephencirving <stephen.c.irving@gmail.com>
2021-12-18 18:07:49 +1100

    3.3 (4:00-7:10) [AirKong] - Instant/Request Booking Function - Added controller: reservations_controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -15,9 +15,19 @@ class ReservationsController < ApplicationController
       @reservation.room = room
       @reservation.price = room.price
       @reservation.total = room.price * days
-      @reservation.save
+      #@reservation.save
 
-      flash[:notice] = "Booked Successfully!"
+      if @reservation.save
+        if room.Request?
+          flash[:notice] = "Request sent successfully!"
+      else
+        @reservation.Approved!
+        flash[:notice] = "Reservation created successfully!"
+      end
+    else
+      flash[:alert] = "Cannot make a reservation"
+    end
+
     end
     redirect_to room
   end
