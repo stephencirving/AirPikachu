5f9a4a8b78b6dd6cbbbe3a7832401f72cc01be3e
stephencirving <stephen.c.irving@gmail.com>
2021-12-19 22:07:44 +1100

    11.3 [AirKong] (no 11.1 or 11.2) - Conversations & Messages Model: conversation.rb, message.rb

---
+++ app/models/message.rb
@@ -0,0 +1,10 @@
+class Message < ApplicationRecord
+  belongs_to :user
+  belongs_to :conversation
+
+  validates_presence_of :content, :conversation_id, :user_id
+
+  def messsage_time
+    self.created_at.strftime(%v)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211218063353) do
+ActiveRecord::Schema.define(version: 20211219104458) do
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "context"
+    t.integer  "user_id"
+    t.integer  "conversation_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "photos", force: :cascade do |t|
     t.integer  "room_id"
