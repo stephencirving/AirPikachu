425f4c2f7b35bea41fe7afc4537c47ad1ef31c77
stephencirving <stephen.c.irving@gmail.com>
2021-12-19 14:48:47 +1100

    3.5 [AirKong] - Reservations status - The booking button didn't work, have messaged leo, will continue

--- db/migrate/20211218063353_add_status_to_reservations.rb
+++ db/migrate/20211218063353_add_status_to_reservations.rb
@@ -1,4 +1,4 @@
-class AddStatusToReservations < ActiveRecord::Migration[5.0]
+class AddStatusToReservation < ActiveRecord::Migration[5.0]
   def change
     add_column :reservations, :status, :integer, default: 0
   end
