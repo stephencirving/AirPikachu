58ab95f908b734d03d85d99952f24cad9e03a5bd
stephencirving <stephen.c.irving@gmail.com>
2021-12-28 20:35:29 +1100

    5.2 [AirKong] (2:36-5:41) - Calendar Availability Model - added has_many :calendars to room.rb, added enum and validates to: calendar.rb and added resources: calendar to routes.rb

---
+++ app/models/calendar.rb
@@ -0,0 +1,6 @@
+class Calendar < ApplicationRecord
+  enum status: [:Available, :Not_Available]
+  validates :day, presence: true
+
+  belongs_to :room
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211219104458) do
+ActiveRecord::Schema.define(version: 20211228092903) do
+
+  create_table "calendars", force: :cascade do |t|
+    t.date     "day"
+    t.integer  "price"
+    t.integer  "status"
+    t.integer  "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_calendars_on_room_id"
+  end
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
