73115dba8b94b1e7071c304fbc6bfe9a1a76d296
stephencirving <stephen.c.irving@gmail.com>
2022-01-03 20:51:38 +1100

    6.3 [AirKong] - Payment Controller - In users_controller.rb created: def payment and def add card - filled in the add card section - Then added get payment_method + post add_card to routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,37 @@ class UsersController < ApplicationController
     redirect_to edit_user_registration_path, alert: "#{e.message}"
   end
 
+  def payment
+  end
+
+  def add_card
+    if current_user.stripe_id.blank?
+      customer = Stripe::Customer.create(
+        email: current_user.email
+      )
+      current_user.stripe_id = customer.id
+      current_user.save
+    else
+      customer = Stripe::Customer.retrieve(current_user.stripe_id)
+    end
+
+    # Add Credit Card to stripe
+    month, year = params[:expiry].split(/ \/ /)
+    new_token = Stripe::Token.create(:card => {
+      :number => params[:number],
+      :exp_month => month,
+      :exp_year => year,
+      :cvv => params[:cvv]
+    })
+    customer.sources.create(source: new_token.id)
+
+    flash[:notice] = "Your card is saved."
+    redirect_to payment_method_path
+  rescue Stripe::CardError => e
+    flash[:alert] = e.message
+    redirect_to payment_method_path
+  end
+
   private
 
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,8 @@ Rails.application.routes.draw do
   end
 
   get '/host_calendar' => "calendars#host"
+  get '/payment_method' => 'users#payment'
+  post '/add_card' => 'users#add_card'
 
   mount ActionCable.server => '/cable'
 
