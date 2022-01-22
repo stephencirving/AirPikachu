bf87f592ea53f0e424fea65079cc2e781869f29b
stephencirving <stephen.c.irving@gmail.com>
2022-01-23 02:56:41 +1100

    14.1 [AirKong] -> Update Room Searching Fucntion -> 1. amend controller to limit search if not available for that night: modified:  app/controllers/pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -41,6 +41,14 @@ class PagesController < ApplicationController
           1
         ).limit(1)
 
+        not_available_in_calendar = Calendar.where(
+          "room_id = ? AND status = ? AND day <= ? AND day >= ?",
+          room.id, 1, end_date, start_date
+        ).limit(1)
+
+				if not_available.length > 0 || not_available_in_calendar.length > 0
+  				@arrRooms.delete(room)
+        end
       end
     end
 
