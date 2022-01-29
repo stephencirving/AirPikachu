95fe7417c287f0cfeb01582b41849ca6affcf4f1
stephencirving <stephen.c.irving@gmail.com>
2022-01-29 20:34:47 +1100

    10.2 [AirKong] -> Settings Model -> 1. Create the new mode: rails g model Setting enable_sms:boolean enable_email:boolean user:references: db/migrate/20220129092041_create_settings.rb. This also creates: app/models/setting.rb, test/fixtures/settings.yml &  test/models/setting_test.rb. 2. Run rails db:migrate: added something to: db/schema.rb. 3. Amend: app/models/user.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220121140416) do
+ActiveRecord::Schema.define(version: 20220129092041) do
 
   create_table "calendars", force: :cascade do |t|
     t.date     "day"
@@ -111,6 +111,15 @@ ActiveRecord::Schema.define(version: 20220121140416) do
     t.float    "longitude"
     t.integer  "instant",      default: 1
     t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
+
+  create_table "settings", force: :cascade do |t|
+    t.boolean  "enable_sms",   default: true
+    t.boolean  "enable_email", default: true
+    t.integer  "user_id"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.index ["user_id"], name: "index_settings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
